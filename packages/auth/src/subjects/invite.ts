afbf1fa5ce091c34d54fc1dc1603ddd5ef0cf080
Vinicius Xavier <vxavier.braga@gmail.com>
2024-05-16 20:16:13 -0300

    feat: add zod and optimizing permission typing

---
+++ packages/auth/src/subjects/invite.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+export const inviteSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),
+    z.literal('delete'),
+  ]),
+  z.literal('Invite'),
+])
+
+export type InviteSubject = z.infer<typeof inviteSubject>
