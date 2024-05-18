290dfdc2661b5b84947ffe2bb0a44ad8c5d8fc24
Vinicius Xavier <vxavier.braga@gmail.com>
2024-05-18 15:34:39 -0300

    feat(routes): create get authenticated profile

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,51 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function getProfile(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/profile',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated  user profile',
+        response: {
+          404: z.object({
+            message: z.string(),
+          }),
+          200: z.object({
+            user: z.object({
+              id: z.string().uuid(),
+              name: z.string().nullable(),
+              email: z.string().email(),
+              avatarUrl: z.string().url().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { sub } = await request.jwtVerify<{ sub: string }>()
+
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+        where: {
+          id: sub,
+        },
+      })
+
+      if (!user) {
+        return reply.status(404).send({ message: 'User not found' })
+      }
+
+      return reply.status(200).send({ user })
+    },
+  )
+}
