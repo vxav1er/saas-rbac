a5a2139866868390823dfb526c5dd9f8d89d1864
Vinicius Xavier <vxavier.braga@gmail.com>
2024-05-19 19:16:35 -0300

    feat(routes): add get organization details

---
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -0,0 +1,52 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+import { BadRequestError } from '../_errors/bad-request'
+
+export async function getOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get details from organizations',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              organization: z.object({
+                id: z.string().uuid(),
+                name: z.string(),
+                slug: z.string(),
+                domain: z.string().nullable(),
+                shouldAttachUsersByDomain: z.boolean(),
+                avatarUrl: z.string().url().nullable(),
+                createdAt: z.date(),
+                updatedAt: z.date(),
+                ownerId: z.string().uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const { organization } = await request.getUserMembership(slug)
+
+        if (!organization) {
+          throw new BadRequestError('Organization not found.')
+        }
+        reply.status(200).send({
+          organization,
+        })
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -20,6 +20,7 @@ import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -63,9 +64,10 @@ app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
-app.register(getMembership)
 
 app.register(createOrganization)
+app.register(getOrganization)
+app.register(getMembership)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
