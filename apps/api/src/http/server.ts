9b9d397a5b16267285a0c70a3d620c0966b6bea8
Vinicius Xavier <vxavier.braga@gmail.com>
2024-05-19 01:46:49 -0300

    feat(swagger): add security to swagger ui

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -32,7 +32,15 @@ app.register(fastifySwagger, {
       description: 'Fullstack Saas app with multi-tenant & RBAC',
       version: '1.0.0',
     },
-    servers: [],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
   },
   transform: jsonSchemaTransform,
 })
