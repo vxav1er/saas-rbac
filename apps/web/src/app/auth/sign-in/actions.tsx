40b7c343d9f6dc1e57637382c45711582c44704f
Vinicius Xavier <vxavier.braga@gmail.com>
2024-07-16 23:35:20 -0300

    feat: add form errors

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,13 +1,51 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
-export async function signInWithEmailAndPassword(data: FormData) {
-  const { email, password } = Object.fromEntries(data)
-  const result = await signInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address.' }),
+  password: z
+    .string()
+    .min(1, { message: 'Please, provide your password.' })
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+})
 
-  console.log(result)
+export async function signInWithEmailAndPassword(_: unknown, data: FormData) {
+  const payload = signInSchema.safeParse(Object.fromEntries(data))
+
+  if (!payload.success) {
+    const errors = payload.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+  const { email, password } = payload.data
+
+  try {
+    const result = await signInWithPassword({
+      email,
+      password,
+    })
+
+    console.log(result)
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(error)
+    return {
+      success: false,
+      message: 'An unexpected error occurred, try again in a few minutes',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
