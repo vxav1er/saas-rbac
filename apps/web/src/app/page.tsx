dd4c735616587990e83d30b028254b3c9b730cba
Vinicius Xavier <vxavier.braga@gmail.com>
2024-07-17 01:05:48 -0300

    feat: get authenticated profile

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+import { auth } from '@/auth/auth'
+
+export default async function Home() {
+  const { user } = await auth()
+  return <h1>{JSON.stringify(user, null, 2)}</h1>
 }
